00dcd320ba7196713f3f775939de2307b8c53adf
Ronald Kiefer <2fixelectronics777@gmail.com>
2025-05-29 03:55:40 -0500

    first mod

---
+++ src/routes/Delete/+page.svelte
@@ -0,0 +1,24 @@
+
+
+<script>
+   //import Search from './Search.svelte';
+    
+</script>
+
+<!-- Example SvelteKit upload form -->
+<form method="POST" enctype="multipart/form-data" action="/api/upload">
+  <input type="file" name="file" accept="video/mp4" required />
+  <button type="submit">Upload Video</button>
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
