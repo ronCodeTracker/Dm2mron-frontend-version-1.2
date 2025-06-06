2771618c30f83cac77e5f5ea8240cbbeb621cb30
Ronald K <36178896+ronCodeTracker@users.noreply.github.com>
2025-06-06 12:16:10 -0500

    toggle update mod

--- src/routes/AnadarkoSurveillance/+page.svelte
+++ src/routes/AnadarkoSurveillance/+page.svelte
@@ -10,7 +10,7 @@
   let videoKeyToDelete = '';
   let fileToUpload = null;
 
-
+  let navOpen = false;
 
   
   onMount(async () => {
@@ -85,9 +85,7 @@ const uploadVideo = () => {};
   };
 */
 
- hamburger.addEventListener('click', () => {
-            navMenu.classList.toggle('hidden');
-        });
+ 
 
 
 </script>
@@ -100,11 +98,13 @@ const uploadVideo = () => {};
 
   <!-- Hamburger and nav at top right -->
     <div class="absolute top-8 right-8 flex flex-col items-end z-20">
-        <button id="hamburger" class="bg-black/60 hover:bg-black/80 rounded-full p-4 shadow-lg text-white focus:outline-none transition-all duration-200">
+        <button id="hamburger" class="bg-black/60 hover:bg-black/80 rounded-full p-4 shadow-lg text-white focus:outline-none transition-all duration-200"
+          on:click={() => navOpen = !navOpen}>
             <svg class="w-12 h-12" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
             </svg>
         </button>
+        {#if navOpen}
         <nav id="nav-menu" class="hidden flex-col items-end mt-4 bg-black/70 rounded-lg p-4 shadow-lg">
             <a href="/SearchandRescue" class="nav-link text-white py-2 px-4 rounded hover:bg-amber-400/80 hover:text-black transition">Search and Rescue</a>
             <a href="/AnadarkoSurveillance" class="nav-link text-white py-2 px-4 rounded hover:bg-amber-400/80 hover:text-black transition">Anadarko Surveillance</a>
@@ -112,6 +112,7 @@ const uploadVideo = () => {};
             <a href="/" class="nav-link text-white py-2 px-4 rounded hover:bg-amber-400/80 hover:text-black transition">Link 4</a>
             <a href="/" class="nav-link text-white py-2 px-4 rounded hover:bg-amber-400/80 hover:text-black transition">Link 5</a>
         </nav>
+        {/if}
     </div>
 
 
